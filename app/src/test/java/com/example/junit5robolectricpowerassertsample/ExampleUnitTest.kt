44bc97b8fd300de88b00699336a97085480baebc
kyanro <kyanro@users.noreply.github.com>
2024-11-24 07:37:11 +0900

    contextを使ったテストがRobolectri上でJUnit5で実行されることを確認

--- app/src/test/java/com/example/junit5robolectricpowerassertsample/ExampleUnitTest.kt
+++ app/src/test/java/com/example/junit5robolectricpowerassertsample/ExampleUnitTest.kt
@@ -1,6 +1,10 @@
 package com.example.junit5robolectricpowerassertsample
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.extension.ExtendWith
+import tech.apter.junit.jupiter.robolectric.RobolectricExtension
 import kotlin.test.Test
 
 
@@ -9,10 +13,18 @@ import kotlin.test.Test
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@ExtendWith(RobolectricExtension::class)
 class ExampleUnitTest {
     @Test
     @DisplayName("デフォルトで用意されてるexampleテスト")
     fun addition_isCorrect() {
         assert(4 == 2 + 2)
+    }
+
+    @Test
+    @DisplayName("package名確認")
+    fun checkPackageName() {
+        val context: Context = ApplicationProvider.getApplicationContext()
+        assert("com.example.junit5robolectricpowerassertsample" == context.packageName)
     }
 }
